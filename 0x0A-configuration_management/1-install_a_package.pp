579d79b1e2730f519e786d709e8c45a6a1f00cef
SamadouOuroagorouko <Souroagorouko@gmail.com>
2023-01-14 15:30:11 +0000

    installing flask package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,20 +1,6 @@
-# Install a package using Puppet
+# Install a Flask package using Puppet
 
-exec { 'install ruby':
-  command => '/usr/bin/apt-get install -y ruby'
-}
-
-exec { 'install puppet-lint':
-  command => '/usr/bin/gem install puppet-lint -v 2.1.1'
-}
-
-package { 'ruby':
-  ensure => 'installed',
-  before => Exec['install ruby']
-}
-
-package { 'puppet-lint':
-  ensure  => 'installed',
-  before  => Exec['install puppet-lint'],
-  require => Package['ruby']
+package { 'flask':
+  ensure => '2.1.0',
+  provider => 'pip3'
 }
