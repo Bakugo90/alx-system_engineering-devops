b85e68a0e65359a2e557f7d7204bdf8715652e85
SamadouOuroagorouko <Souroagorouko@gmail.com>
2023-01-16 23:07:52 +0000

    deleting

--- 0x10-python-network_0/0-body_size.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash env
-#Connect to an url and display the size of body header
-
-url=$1
-response=$(curl -s -o /dev/null -w "%{size_download}" $url)
-echo "$response"
--- 0x10-python-network_0/README.md
+++
@@ -1,1 +0,0 @@
--0x10. Python - Network #0
