5ad573973d9bb093dfb0473d0aa091a86a220ecb
SamadouOuroagorouko <Souroagorouko@gmail.com>
2023-01-21 23:41:50 +0000

    changing my rsa key

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -11,6 +11,6 @@ file_line { 'Turn off passwd auth':
 file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/holberton',
+  line   => 'IdentityFile ~/.ssh/school',
   match  => '^IdentityFile'
 }
